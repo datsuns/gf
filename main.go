7c2ab4a0c07f4d4539ddfcecb0b05e1bcbc22aad
datsuns <the.skeleton7@gmail.com>
2024-03-21 23:18:57 +0900

    add: simple filer
    * cant show multibyte text...

--- main.go
+++ main.go
@@ -7,22 +7,101 @@ import (
 	"github.com/gizak/termui/v3/widgets"
 )
 
+func genPane(app *App, side PaneSide) *widgets.List {
+	w := widgets.NewList()
+	p := app.Panes[side]
+	p.Widget = w
+	p.Reload()
+
+	if side == LeftPane {
+		w.SetRect(0, 00, 60, 30)
+	} else {
+		w.SetRect(60, 00, 120, 30)
+	}
+	return w
+}
+
+func rollbackSelected(w *widgets.List) {
+	w.SelectedRowStyle = ui.NewStyle(ui.ColorWhite)
+	ui.Render(w)
+}
+
+func changeTo(app *App, side PaneSide) *Pane {
+	app.Current = side
+	ret := app.Panes[app.Current]
+	ret.Widget.SelectedRowStyle = ui.NewStyle(ui.ColorYellow)
+	ui.Render(ret.Widget)
+	return ret
+}
+
+func mainHandler(app *App) {
+	uiEvents := ui.PollEvents()
+	for {
+		e := <-uiEvents
+		pane := app.Panes[app.Current]
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		case "j", "<Down>":
+			pane.Widget.ScrollDown()
+		case "k", "<Up>":
+			pane.Widget.ScrollUp()
+		case "l":
+			rollbackSelected(pane.Widget)
+			pane = changeTo(app, RightPane)
+		case "h":
+			rollbackSelected(pane.Widget)
+			pane = changeTo(app, LeftPane)
+		case "u":
+			if err := pane.Dir.Up(); err == nil {
+				pane.Widget.SelectedRow = 0
+				pane.Reload()
+			}
+			ui.Render(pane.Widget)
+		case "<Enter>":
+			if err := pane.Dir.Down(Path(pane.Selected())); err == nil {
+				pane.Widget.SelectedRow = 0
+				pane.Reload()
+			}
+			ui.Render(pane.Widget)
+		}
+
+		if pane.DirSelected() {
+			pane.Widget.SelectedRowStyle = ui.NewStyle(ui.ColorCyan)
+		} else {
+			pane.Widget.SelectedRowStyle = ui.NewStyle(ui.ColorYellow)
+		}
+		ui.Render(pane.Widget)
+	}
+}
+
 func main() {
+	var err error
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
 
-	p := widgets.NewParagraph()
-	p.Text = "Hello World!"
-	p.SetRect(0, 0, 25, 5)
-
-	ui.Render(p)
-
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			log.Println(e.ID)
-			break
-		}
+	cfg, err := LoadConfig("gf.toml")
+	if err != nil {
+		panic(err)
 	}
+	app, err := NewApp(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	app.Current = LeftPane
+	genPane(app, LeftPane)
+	genPane(app, RightPane)
+	pane := app.Panes[app.Current]
+	if pane.DirSelected() {
+		pane.Widget.SelectedRowStyle = ui.NewStyle(ui.ColorCyan)
+	} else {
+		pane.Widget.SelectedRowStyle = ui.NewStyle(ui.ColorYellow)
+	}
+
+	ui.Render(app.Panes[LeftPane].Widget)
+	ui.Render(app.Panes[RightPane].Widget)
+	mainHandler(app)
 }
--- pane.go
+++ pane.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/gizak/termui/v3/widgets"
+)
+
 type PaneSide int
 
 const (
@@ -8,7 +12,8 @@ const (
 )
 
 type Pane struct {
-	Dir *Dir
+	Dir    *Dir
+	Widget *widgets.List
 }
 
 func NewPane(path Path) (*Pane, error) {
@@ -17,4 +22,31 @@ func NewPane(path Path) (*Pane, error) {
 		return nil, err
 	}
 	return &Pane{Dir: d}, nil
+}
+
+func (p *Pane) Cur() string {
+	return p.Dir.Cur()
+}
+
+func (p *Pane) Selected() string {
+	return p.Widget.Rows[p.Widget.SelectedRow]
+}
+
+func (p *Pane) DirSelected() bool {
+	if len(p.Widget.Rows) == 0 {
+		return false
+	}
+	return p.Selected()[len(p.Selected())-1] == '/'
+}
+
+func (p *Pane) Reload() {
+	p.Widget.Title = p.Dir.Cur()
+	p.Widget.Rows = []string{}
+	for _, f := range p.Dir.Entries {
+		if f.IsDir() {
+			p.Widget.Rows = append(p.Widget.Rows, f.Name()+"/")
+		} else {
+			p.Widget.Rows = append(p.Widget.Rows, f.Name())
+		}
+	}
 }
